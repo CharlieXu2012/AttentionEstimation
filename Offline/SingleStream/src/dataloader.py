bd1cd0c51ad761cbbe86c3e71cbbefe3b2f8307a
Gary Corcoran <gcorc@cim.mcgill.ca>
2018-04-23 15:11:46 -0400

    Updated to work with processed videos, and removed flow.

--- Offline/SingleStream/src/dataloader.py
+++ Offline/SingleStream/src/dataloader.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import numbers
 import numpy as np
-import cv2
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import WeightedRandomSampler
 from torchvision import transforms
@@ -11,14 +10,12 @@ class AppearanceDataset(Dataset):
 
     Args:
         labels_path (string):   path to text file with annotations
-        sequence_len (int):     length of video clip
-        sample_rate (int):      sample video
         transform (callable):   transform to be applied on video sequence
         
     Returns:
         torch.utils.data.Dataset:   dataset object
     """
-    def __init__(self, labels_path, sequence_len, sample_rate, transform=None):
+    def __init__(self, labels_path, transform=None):
         # read video paths and labels
         with open(labels_path, 'r') as f:
             data = f.read()
@@ -27,106 +24,19 @@ class AppearanceDataset(Dataset):
             data = np.reshape(data, (-1, 2))
         
         self.data = data
-        self.sequence_len = sequence_len
-        self.sample_rate = sample_rate
         self.transform = transform
-        self.cap = cv2.VideoCapture()
     
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         # create list to hold video data
-        X = []
         y = int(self.data[idx, 1]) - 1
-        video_path = 'data/' + self.data[idx, 0]
-        # open video
-        self.cap.open(video_path)
-        # set to beginning of sequence
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, 100-self.sequence_len)
-        for i in range(self.sequence_len):
-            _, frame = self.cap.read()
-            # sample sequence
-            if i % self.sample_rate == 0:
-                frame = cv2.resize(frame, (256,256))
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                X.append(frame)
-
-        # convert to numpy array
-        X = np.array(X)
-        # transform data
-        if self.transform:
-            X = self.transform(X)
-        # reformat [numSeqs x numChannels x Height x Width]
-        X = np.transpose(X, (0,3,1,2))
-        # store in sample
-        sample = {'X': X, 'y': y}
-        return sample
-
-class FlowDataset(Dataset):
-    """Flow Features for Attention Level Dataset.
-    Args:
-        labels_path (string):   path to text file with annotations
-        sequence_len (int):     length of video clip
-        sample_rate (int):      sample video
-        transform (callable):   transform to be applied on video sequence
-
-    Returns:
-        torch.utils.data.Dataset:   dataset object
-    """ 
-    def __init__(self, labels_path, sequence_len, sample_rate, transform=None):
-        # read video paths and labels
-        with open(labels_path, 'r') as f:
-            data = f.read()
-            data = data.split()
-            data = np.array(data)
-            data = np.reshape(data, (-1, 2))
-        
-        self.data = data
-        self.sequence_len = sequence_len
-        self.sample_rate = sample_rate
-        self.transform = transform
-        self.cap = cv2.VideoCapture()
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        # create list to hold video data
-        X = []
-        y = int(self.data[idx, 1]) - 1
-        video_path = 'data/' + self.data[idx, 0]
-        # open video
-        self.cap.open(video_path)
-        # set to beginning of sequence
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, 100-self.sequence_len)
-        # read initial frame
-        _, frame1 = self.cap.read()
-        frame1 = cv2.resize(frame1, (256,256))
-        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-        for i in range(1, self.sequence_len):
-            _, frame2 = self.cap.read()
-            if i % self.sample_rate == 0:
-                frame2 = cv2.resize(frame2, (256,256))
-                frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-                # compute flow
-                flow = cv2.calcOpticalFlowFarneback(frame1, frame2, None,
-                        0.5, 3, 15, 3, 5, 1.2, 0)
-                # compute flow mag
-                mag, _ = cv2.cartToPolar(flow[:,:,0], flow[:,:,1])
-                # normalize
-                flow[:,:,0] = cv2.normalize(flow[:,:,0], None, 0, 255,
-                        cv2.NORM_MINMAX)
-                flow[:,:,1] = cv2.normalize(flow[:,:,1], None, 0, 255,
-                        cv2.NORM_MINMAX)
-                mag = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-                # normalize between 0-1
-                X.append(np.stack((flow[:,:,0], flow[:,:,1], mag),axis=2))
-
-                frame1 = frame2
-
-        # convert to numpy array
-        X = np.array(X)
+        video_path = 'data/offline/' + self.data[idx, 0]
+        video_path = video_path[:-3] + 'npy'
+        print(video_path)
+        # load video
+        X = np.load(video_path)
         # transform data
         if self.transform:
             X = self.transform(X)
@@ -248,32 +158,6 @@ class RandomRotation():
         
         return video_new
 
-class RandomWindow():
-    """Window video sequence.
-
-    Args:
-        window_size (int):   Length of window..
-    """
-    def __init__(self, window_size):
-        assert isinstance(window_size, int)
-        self.window_size = window_size
-
-    def __call__(self, video):
-        """
-        Args:
-            video (ndarray):    Video to be sampled.
-
-        Returns:
-            ndarray: Randomly sampled video.
-        """
-        print('Video:', video.shape)
-        if self.window_size >= video.shape[0]:
-            return video
-
-        start = np.random.randint(video.shape[0] - self.window_size)
-        video_new = video[start:start+self.window_size]
-        return video_new
-
 class Normalize():
     """Normalize video with mean and standard deviation.
     Given mean: ``(M1,...,Mn)`` and std: ``(S1,..,Sn)`` for ``n`` channels, this
@@ -320,18 +204,13 @@ def count_labels(data_path):
 
     return counts
 
-def get_loaders(train_path, valid_path, batch_size, sequence_len,
-        sample_rate, flow, num_workers, gpu):
+def get_loaders(train_path, valid_path, batch_size, num_workers, gpu):
     """Return dictionary of torch.utils.data.DataLoader.
 
     Args:
         train_path (string):    path to training annotations
         valid_path (string):    path to validation annotations
         batch_size (int):       number of instances in batch
-        sequence_len (int):     number of frames to keep in video (counting
-                                    from last frame)
-        sample_rate (int):    sample video sequence
-        flow (bool):            use flow or appearance dataset
         num_workers (int):      number of subprocesses used for data loading
         gpu (bool):             presence of gpu
 
@@ -356,37 +235,33 @@ def get_loaders(train_path, valid_path, batch_size, sequence_len,
             }
 
     # create dataset object
-    if flow:
-        datasets = {
-                'Train': FlowDataset(train_path, sequence_len,
-                    sample_rate, data_transforms['Train']),
-                'Valid': FlowDataset(valid_path, sequence_len,
-                    sample_rate, data_transforms['Valid'])
-                }
-    else:
-        datasets = {
-                'Train': AppearanceDataset(train_path, sequence_len,
-                    sample_rate, data_transforms['Train']),
-                'Valid': AppearanceDataset(valid_path, sequence_len,
-                    sample_rate, data_transforms['Valid'])
-                }
+    datasets = {
+            'Train': AppearanceDataset(train_path, data_transforms['Train']),
+            'Valid': AppearanceDataset(valid_path, data_transforms['Valid'])
+            }
 
     # dataset sizes
     dataset_sizes = {'Train': len(datasets['Train']),
                      'Valid': len(datasets['Valid'])}
 
     # add weighted sampler since unbalanced dataset
-    c = count_labels(train_path)
-    weights = np.zeros(dataset_sizes['Train'])
-    weights[: c[0]] = c[1] / c[0]
-    weights[c[0]: c[0]+c[1]] = 1
-    weights[c[0]+c[1]:c[0]+c[1]+c[2]] = c[1] / c[2]
-    weights[c[0]+c[1]+c[2]:] = c[1] / c[3]
+#    c = count_labels(train_path)
+#    weights = np.zeros(dataset_sizes['Train'])
+#    weights[: c[0]] = c[1] / c[0]
+#    weights[c[0]: c[0]+c[1]] = 1
+#    weights[c[0]+c[1]:c[0]+c[1]+c[2]] = c[1] / c[2]
+#    weights[c[0]+c[1]+c[2]:] = c[1] / c[3]
 
     # create dataloders
+#    dataloaders = {
+#            'Train': DataLoader(datasets['Train'], batch_size=batch_size,
+#                sampler=WeightedRandomSampler(weights, dataset_sizes['Train'])),
+#            'Valid': DataLoader(datasets['Valid'], batch_size=batch_size,
+#                shuffle=True)
+#            }
     dataloaders = {
             'Train': DataLoader(datasets['Train'], batch_size=batch_size,
-                sampler=WeightedRandomSampler(weights, dataset_sizes['Train'])),
+                shuffle=True),
             'Valid': DataLoader(datasets['Valid'], batch_size=batch_size,
                 shuffle=True)
             }
@@ -404,13 +279,10 @@ def main():
     test_path = 'data/test_data.txt'
     # hyper-parameters
     batch_size = 2
-    flow = True
-    sequence_len = 100  # counting backwards from last frame
-    sample_rate = 5
-    
+    num_workers = 2
+
     dataloaders, dataset_sizes = get_loaders(train_path, valid_path, 
-            batch_size, sequence_len, sample_rate, flow, num_workers=0, 
-            gpu=False)
+            batch_size, num_workers, gpu=True)
     print('Dataset Sizes:')
     print(dataset_sizes)
 
@@ -429,7 +301,7 @@ def main():
     data, labels = train_batch['X'], train_batch['y']
     print('data:', data.shape)
     print('labels:', labels.shape)
-    grid = utils.make_grid(data[0])
+    grid = utils.make_grid(data[0], nrow=10)
     imshow(grid)
 
 if __name__ == '__main__':
